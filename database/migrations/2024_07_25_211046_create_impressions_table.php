790da25e81fb38eacad52deead61a58389f301b0
Arindam Kabir <arindamkabir@gmail.com>
2024-07-27 03:08:09 +0600

    feat: add / update migrations

---
+++ database/migrations/2024_07_25_211046_create_impressions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('impressions', function (Blueprint $table) {
+            $table->id();
+            $table->string('ip')->nullable();
+            $table->string('user_agent')->nullable();
+            $table->unsignedBigInteger('impressionable_id');
+            $table->string('impressionable_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('impressions');
+    }
+};
