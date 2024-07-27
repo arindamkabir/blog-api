a32daec00822602113ba49ac3c411bb355cb2e58
Arindam Kabir <arindamkabir@gmail.com>
2024-07-27 16:51:41 +0600

    feat: rename trait

--- app/Traits/WithGeneratedUniqueSlugTrait.php
+++ app/Traits/WithGeneratedUniqueSlugTrait.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace App\Traits;
 
-trait GenerateUniqueSlugTrait
+trait WithGeneratedUniqueSlugTrait
 {
-    public static function bootGenerateUniqueSlugTrait(): void
+    public static function bootWithGeneratedUniqueSlugTrait(): void
     {
         static::saving(function ($model) {
-            if ($model->isDirty('name')) {
-                $slug = str()->slug($model->name);
+            if ($model->isDirty('title')) {
+                $slug = str()->slug($model->title);
                 $count = $model->where('id', '!=', $model->id)
                     ->whereRaw("slug RLIKE '^{$slug}(-[0-9]+)?$'")
                     ->count();
